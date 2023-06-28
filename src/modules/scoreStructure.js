453be9fdf9b785079cc8b87fb7bb4aa1c2dd3c2d
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-06-27 22:17:50 -0300

    add style

--- src/modules/scoreStructure.js
+++ src/modules/scoreStructure.js
@@ -2,15 +2,15 @@ const itemScore = () => {
   const scoreslist = document.querySelector('#scores-list');
   scoreslist.classList.add('score-box');
   scoreslist.innerHTML = `
-    <li class="list" id="1"><p class="name">Rod: </p><p>100</p></li>
-    <li class="list" id="2"><p class="name">Mason: </p><p> 99</p></li>
-    <li class="list" id="3"><p class="name">Martin: </p><p>20</p></li>
-    <li class="list" id="4"><p class="name">Hugo: </p><p>100</p></li>
-    <li class="list" id="5"><p class="name">Jadnael: </p><p>100</p></li>
-    <li class="list" id="6"><p class="name">Luis: </p><p>100</p></li>
-    <li class="list" id="7"><p class="name">Alfred: </p><p>100</p></li>
-    <li class="list" id="8"><p class="name">Gabriel: </p><p>100</p></li>
-    <li class="list" id="9"><p class="name">Gonzalo: </p><p>100</p></li>
+    <li class="score-item" id="1"><p class="name">Rod:</p><p>100</p></li>
+    <li class="score-item" id="2"><p class="name">Mason: </p><p> 99</p></li>
+    <li class="score-item" id="3"><p class="name">Martin: </p><p>20</p></li>
+    <li class="score-item" id="4"><p class="name">Hugo: </p><p>100</p></li>
+    <li class="score-item" id="5"><p class="name">Jadnael: </p><p>100</p></li>
+    <li class="score-item" id="6"><p class="name">Luis: </p><p>100</p></li>
+    <li class="score-item" id="7"><p class="name">Alfred: </p><p>100</p></li>
+    <li class="score-item" id="8"><p class="name">Gabriel: </p><p>100</p></li>
+    <li class="score-item" id="9"><p class="name">Gonzalo: </p><p>100</p></li>
   `;
 };
 
